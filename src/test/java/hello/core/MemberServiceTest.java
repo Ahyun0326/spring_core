24d7af4879c0a72199e78ec07bd78e59c763f3de
Yeseo <chinkl0110@naver.com>
2023-08-24 22:03:14 +0900

    관심사의 분리

--- src/test/java/hello/core/MemberServiceTest.java
+++ src/test/java/hello/core/MemberServiceTest.java
@@ -3,13 +3,19 @@ package hello.core;
 import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
-import hello.core.member.MemberServiceImpl;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class MemberServiceTest {
 
-    MemberService memberService = new MemberServiceImpl();
+    MemberService memberService;
+
+    @BeforeEach // 테스트를 실행하기 전에 무조건 실행
+    public void beforeEach() {
+        AppConfig appConfig = new AppConfig();
+        memberService = appConfig.memberService();
+    }
 
     @Test
     void join(){
